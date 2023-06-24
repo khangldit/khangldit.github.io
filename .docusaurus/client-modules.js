4a62a8d8b1f469b487b82d5a4dc24bfd0fdd14c5
KhangLD <khangldit@gmail.com>
2023-06-24 16:05:16 +0700

    Deploy website - based on 58ae11945b3cd5cf722803e15e87e197bb5d721b

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/khangld/Tanca/khangldit.github.io/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/khangld/Tanca/khangldit.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/khangld/Tanca/khangldit.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/khangld/Tanca/khangldit.github.io/src/css/custom.css'),
-];
